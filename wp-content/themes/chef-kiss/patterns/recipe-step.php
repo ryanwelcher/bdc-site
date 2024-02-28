5b956a3a00c22141dd64db3b1470e57d44fa6b79
Ryan Welcher <me@ryanwelcher.com>
2024-02-28 15:43:37 -0500

    Add pattern for recipe steps.

---
+++ wp-content/themes/chef-kiss/patterns/recipe-step.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Recipe Step
+ * Slug: chef-kiss/recipe-step
+ * Description: Defines a step for a recipe
+ * Block Types: core/group
+ */
+
+ ?>
+<!-- wp:group {"metadata":{},"style":{"border":{"radius":{"topLeft":"1rem"}},"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"},"margin":{"bottom":"3rem"}}},"backgroundColor":"background","className":"recipe-container","layout":{"type":"constrained"}} -->
+<div class="wp-block-group recipe-container has-background-background-color has-background" style="border-top-left-radius:1rem;margin-bottom:3rem;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
