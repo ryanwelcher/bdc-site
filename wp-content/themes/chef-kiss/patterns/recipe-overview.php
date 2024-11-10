045fa394b8d1a6bad952d00f088b5dde9cb13158
Ryan Welcher <me@ryanwelcher.com>
2024-11-10 10:57:56 -0500

    Add default text.

--- wp-content/themes/chef-kiss/patterns/recipe-overview.php
+++ wp-content/themes/chef-kiss/patterns/recipe-overview.php
@@ -10,7 +10,7 @@
 ?>
 <!-- wp:group {"metadata":{},"style":{"border":{"radius":{"topLeft":"1rem"}},"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"},"margin":{"bottom":"3rem"}}},"backgroundColor":"background","className":"recipe-container","layout":{"type":"constrained"}} -->
 <div class="wp-block-group recipe-container has-background-background-color has-background" style="border-top-left-radius:1rem;margin-bottom:3rem;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:heading -->
-<h2 class="wp-block-heading"></h2>
+<h2 class="wp-block-heading">Overview</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"chef-kiss/excerpt"}}}} -->
