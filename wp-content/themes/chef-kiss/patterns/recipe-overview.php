78b2df1ab4fe960078476388dc1261b2ba454c6a
Ryan Welcher <me@ryanwelcher.com>
2024-11-10 10:55:42 -0500

    Add recipe overview pattern.

---
+++ wp-content/themes/chef-kiss/patterns/recipe-overview.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Recipe Overview
+ * Slug: chef-kiss/recipe-overview-step
+ * Description: Defines a step for a recipe
+ * Block Types: core/group
+ * Categories: chef-kiss/conference-voting
+ */
+
+?>
+<!-- wp:group {"metadata":{},"style":{"border":{"radius":{"topLeft":"1rem"}},"spacing":{"padding":{"top":"2rem","bottom":"2rem","left":"2rem","right":"2rem"},"margin":{"bottom":"3rem"}}},"backgroundColor":"background","className":"recipe-container","layout":{"type":"constrained"}} -->
+<div class="wp-block-group recipe-container has-background-background-color has-background" style="border-top-left-radius:1rem;margin-bottom:3rem;padding-top:2rem;padding-right:2rem;padding-bottom:2rem;padding-left:2rem"><!-- wp:heading -->
+<h2 class="wp-block-heading"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"chef-kiss/excerpt"}}}} -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
