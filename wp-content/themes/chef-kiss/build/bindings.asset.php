bd987c02fa9489881f9cf6930ef310e8062819f7
Ryan Welcher <me@ryanwelcher.com>
2024-11-12 18:19:41 -0500

    Rebuild bindings.

--- wp-content/themes/chef-kiss/build/bindings.asset.php
+++ wp-content/themes/chef-kiss/build/bindings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-core-data', 'wp-i18n'), 'version' => '6ee006fd63db93d83831');
+<?php return array('dependencies' => array('wp-blocks', 'wp-core-data', 'wp-i18n'), 'version' => '57427476209767813c84');
