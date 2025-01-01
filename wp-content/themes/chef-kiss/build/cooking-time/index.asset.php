968f30381aaf989e7b41ecef5e4ae1ae4c433b27
Ryan Welcher <me@ryanwelcher.com>
2025-01-01 14:01:24 -0500

    Rebuild.

--- wp-content/themes/chef-kiss/build/cooking-time/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-i18n'), 'version' => '4a702f5b91dddea6b212');
--- wp-content/themes/chef-kiss/build/level/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-i18n'), 'version' => '11ea7c89fe35e8977f27');
